ceb3d6de33143b840a141248960ed64a5aedd77d
Alex Grieco <agrieco@pm.me>
2023-10-18 19:45:20 -0700

    Adjust file download location for utils

--- zsh/.zsh/downloads.zsh
+++ zsh/.zsh/downloads.zsh
@@ -17,9 +17,9 @@ fi
     git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 
 # Download Catppuccin Theme
-[[ -d ~/important/catppuccin/iterm ]] ||
+[[ -d ~/.utils/catppuccin/iterm ]] ||
   git clone --depth 1 -- \
-    git@github.com:catppuccin/iterm.git ~/important/catppuccin/iterm/
+    git@github.com:catppuccin/iterm.git ~/.utils/catppuccin/iterm/
 
 # Install Starship if not already installed
 if ! [ -x "$(command -v starship)" ]; then
@@ -27,11 +27,11 @@ if ! [ -x "$(command -v starship)" ]; then
 fi
 
 # Download Znap, if it's not there yet.
-[[ -f ~/Git/zsh-snap/znap.zsh ]] ||
+[[ -f ~/.utils/zsh-snap/znap.zsh ]] ||
     git clone --depth 1 -- \
-        https://github.com/marlonrichert/zsh-snap.git ~/Git/zsh-snap
+        https://github.com/marlonrichert/zsh-snap.git ~/.utils/zsh-snap
 ##
 # Source Znap at the start of your .zshrc file.
 #
-source ~/Git/zsh-snap/znap.zsh
+source ~/.utils/zsh-snap/znap.zsh
 
