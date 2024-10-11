53e4f9b51b9521bba5b693c9e8822501143ef93b
Alex Grieco <awgrieco@gmail.com>
2024-10-11 14:59:48 -0700

    Fixing commentstring issue for terraform

--- config/.config/nvim/lua/plugins/mini.lua
+++ config/.config/nvim/lua/plugins/mini.lua
@@ -45,5 +45,15 @@ return { -- Collection of various small independent plugins/modules
     end
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
+
+
+    -- Fix terraform and hcl comment string
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+      callback = function(ev)
+        vim.bo[ev.buf].commentstring = "# %s"
+      end,
+      pattern = { "terraform", "hcl" },
+    })
   end,
 }
