3ba47c77fa09a1ca46f97baba69d3bddede0a6ef
Alex Grieco <awgrieco@gmail.com>
2025-01-10 10:42:35 -0800

    Reorg zshrc for readability

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,46 +1,11 @@
-# Sesh settings
-# Adds keybinding and function to run sesh
-function sesh-sessions() {
-  {
-    exec </dev/tty
-    exec <&1
-    local session
-    session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ' \
-      --header ' ^a all ^d kill ^f find ^t tmux ^g configs ^x zoxide ' \
-      --bind 'tab:down,btab:up' \
-      --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list)' \
-      --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c)' \
-      --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z)' \
-      --bind 'ctrl-f:change-prompt(🔎  )+reload(find ~ ~/Documents/B_Product_Development ~/Nextcloud/Development ~/Nextcloud/Development/golang/alexander-grieco ~/dapper -mindepth 1 -maxdepth 1 -type d )' \
-      --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t)' \
-      --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list)'
-    )
-    zle reset-prompt > /dev/null 2>&1 || true
-    [[ -z "$session" ]] && return
-    sesh connect $session
-  }
-}
-
-zle     -N             sesh-sessions
-bindkey -M emacs '^f' sesh-sessions
-bindkey -M vicmd '^f' sesh-sessions
-bindkey -M viins '^f' sesh-sessions
-# End Sesh settings
-
-if [[ $_distro == "macos" ]]; then
-  alias ls="gls --color --group-directories-first"
-  # alias ls="ls"
-  alias cdd="cd ~/Documents/B_Product_Development/"
-else
-  alias ls="ls --color --group-directories-first"
-fi
-
+# Misc aliases
 alias python="python3"
 alias ll="ls -lhaG"
 alias c="clear"
 alias sb="source ~/.zshrc"
 alias vb="vim ~/.zshrc"
 alias ddd="cd $HOME/dapper"
+alias cdn="cd ~/Nextcloud/Development"
 
 # Vim
 alias vim="nvim"
@@ -87,3 +52,40 @@ alias zj="zellij"
 
 # zoxide
 alias z="zoxide"
+
+# Sesh settings
+# Adds keybinding and function to run sesh
+function sesh-sessions() {
+  {
+    exec </dev/tty
+    exec <&1
+    local session
+    session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ' \
+      --header ' ^a all ^d kill ^f find ^t tmux ^g configs ^x zoxide ' \
+      --bind 'tab:down,btab:up' \
+      --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list)' \
+      --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c)' \
+      --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z)' \
+      --bind 'ctrl-f:change-prompt(🔎  )+reload(find ~ ~/Documents/B_Product_Development ~/Nextcloud/Development ~/Nextcloud/Development/golang/alexander-grieco ~/dapper -mindepth 1 -maxdepth 1 -type d )' \
+      --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t)' \
+      --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list)'
+    )
+    zle reset-prompt > /dev/null 2>&1 || true
+    [[ -z "$session" ]] && return
+    sesh connect $session
+  }
+}
+
+zle     -N             sesh-sessions
+bindkey -M emacs '^f' sesh-sessions
+bindkey -M vicmd '^f' sesh-sessions
+bindkey -M viins '^f' sesh-sessions
+# End Sesh settings
+
+if [[ $_distro == "macos" ]]; then
+  alias ls="gls --color --group-directories-first"
+  # alias ls="ls"
+  alias cdd="cd ~/Documents/B_Product_Development/"
+else
+  alias ls="ls --color --group-directories-first"
+fi
