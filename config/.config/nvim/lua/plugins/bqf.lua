abb588e77cefda8a3c3e9f674f1de92dc7a1481d
Alex Grieco <>
2025-06-08 10:18:23 -0700

    Testing lazyvim config
    This is a test I have decided to undertake by transferring my
    configuration over to a Lazyvim base. While my config is great, I feel
    it is lacking some features that natively come with Lazyvim, and Lazyvim
    still gives me the flexibility to customize where I want.

--- config/.config/nvim/lua/plugins/bqf.lua
+++
@@ -1,6 +0,0 @@
--- Docs for keybindings:
--- https://github.com/kevinhwang91/nvim-bqf
--- BQF - Better Quickfix
-return {
-  "kevinhwang91/nvim-bqf",
-}
--- config/.config/nvim/lua/plugins/present.lua
+++
@@ -1,8 +0,0 @@
-return {
-  dir = vim.fn.stdpath("config") .. "/lua/config/present",
-  config = function()
-    vim.api.nvim_create_user_command("PresentStart", function()
-      require("config.present.present").start_presentation()
-    end, {})
-  end,
-}
