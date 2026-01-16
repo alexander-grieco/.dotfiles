3a303b9551e579b79634454290d81e7c217b6386
Alex Grieco <awgrieco@gmail.com>
2026-01-16 08:28:25 -0700

    Updates and small noice temp disable to fix compatibility issue

---
+++ config/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        -- TODO: Remove this entire config once noice plays nicely with neovim 11
+        view = false,
+      },
+    },
+  },
+}
